92093a0725ecbdc3c33911011255aa8997bedbaa
danusputra <danusputraa@gmail.com>
2022-09-17 15:28:34 +0700

    revisi3

--- percobaan.py
+++ percobaan.py
@@ -11,3 +11,12 @@ menu()
 def menu():
     print ('Menu:\n1.Apakah ingin mengubah data konversi mata uang? \n2.Apakah ingin mengkonversi mata uang? \n3.Apakah ingin keluar?' )
 menu()
+def menu():
+    print ('Menu:\n1.Apakah ingin mengubah data konversi mata uang? \n2.Apakah ingin mengkonversi mata uang? \n3.Apakah ingin keluar?' )
+menu()
+def menu():
+    print ('Menu:\n1.Apakah ingin mengubah data konversi mata uang? \n2.Apakah ingin mengkonversi mata uang? \n3.Apakah ingin keluar?' )
+menu()
+def menu():
+    print ('Menu:\n1.Apakah ingin mengubah data konversi mata uang? \n2.Apakah ingin mengkonversi mata uang? \n3.Apakah ingin keluar?' )
+menu()
